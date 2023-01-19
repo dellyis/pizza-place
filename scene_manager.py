e217640dac3b6276d94677a28a7fc0a73ed5f04d
Dellyis <64022121+Dellyis@users.noreply.github.com>
2023-01-19 23:00:03 +0700

    Create scene_manager.py

---
+++ scene_manager.py
@@ -0,0 +1,43 @@
+from typing import Dict, Optional
+
+import pygame as pg
+
+from constants import FONT
+from utils import load_image
+
+
+class BaseScene:
+    def __init__(self, screen):
+        self.screen = screen
+
+    def draw(self):
+        ...  # Переопределяется для каждой сцены отдельно
+
+
+class HomeScene(BaseScene):
+    ...
+
+
+class SceneManager:
+    def __init__(self):
+        self.scenes: Dict[str, BaseScene] = {}
+        self.current_scene: Optional[BaseScene] = None
+
+    def add(self, scene: BaseScene):
+        if not isinstance(scene, BaseScene):
+            raise TypeError("Сцена должна наследоваться от класса BaseScene")
+        if scene in self.scenes:
+            raise ValueError("Сцена уже добавлена")
+        self.scenes[scene.__class__.__name__] = scene
+
+    def change_scene(self, name: str):
+        if not self.scenes:
+            raise Exception("Нет добавленных сцен")
+        if name not in self.scenes:
+            raise ValueError("Указанной сцены не существует")
+        self.current_scene = self.scenes[name]
+
+    def draw(self):
+        if not self.current_scene:
+            raise Exception("Не выбрана текущая сцена")
+        self.current_scene.draw()
