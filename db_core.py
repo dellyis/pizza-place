7a81679ad32abc64a394af52b7142c6f555c9941
Arturata <104503710+Arturata@users.noreply.github.com>
2023-01-19 16:30:19 +0000

    Create db_core.py

---
+++ db_core.py
@@ -0,0 +1,9 @@
+import json
+
+
+class NiceDocument:
+    def __init__(self, file_name: str):
+        with open(file_name, encoding="UTF-8") as f:
+            self.data = json.loads(f.read())
+        for k, v in self.data.items():
+            setattr(self, k, v)
