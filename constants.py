7f649c5c9a7211ccfbb1b4787f1baf305f6a288a
Arturata <104503710+Arturata@users.noreply.github.com>
2023-01-19 16:08:43 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@ FONT = "./data/ProstoOne.ttf"
 
 
 class Colors:
-    primary: hex
-    danger: hex
-    success: hex
-    warning: hex
-    info: hex
+    primary: hex = 0x3d1313
+    danger: hex = 0xff0000
+    success: hex = 0x00ff00
+    warning: hex = 0xff9b00
+    info: hex = 0x0000ff
