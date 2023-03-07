c3b05a36e41108e9f2ab2f33cf6db376b6fb254b
Arturata <104503710+Arturata@users.noreply.github.com>
2023-03-07 07:55:08 +0000

    обновлены модели бд

--- db_models.py
+++ db_models.py
@@ -7,7 +7,25 @@ class Upgrades(BaseModel):
     animator: bool = False
 
 
+class Ingredients(BaseModel):
+    anchovy: bool = False
+    avocado: bool = False
+    cheese: bool = True
+    cucumber: bool = False
+    grass: bool = False
+    mango: bool = False
+    mushroom: bool = False
+    olives: bool = False
+    pepper: bool = False
+    pineapple: bool = False
+    sausage: bool = False
+    tomato: bool = False
+    tomato_sauce: bool = True
+
+
 class Data(BaseModel):
     money: int = 0
     gems: int = 0
+    day: int = 1
     upgrades: Upgrades
+    ingredients: Ingredients
