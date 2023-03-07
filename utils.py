eb3f51f327e933d16e0d045c39fb8a6d2ef7d9bf
Arturata <104503710+Arturata@users.noreply.github.com>
2023-03-07 11:33:58 +0000

    добавлена документация

--- utils.py
+++ utils.py
@@ -5,7 +5,12 @@ import pygame as pg
 
 
 def load_image(name):
-    fullname = os.path.join('data', name)
+    """
+    Загрузка изображения
+
+    :param name: название изображения
+    """
+    fullname = os.path.join("data", name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
