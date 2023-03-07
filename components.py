2cffb4a72ed23affa1f7de3cf8cf471a00f4f3d7
Arturata <104503710+Arturata@users.noreply.github.com>
2023-03-07 08:51:18 +0000

    добавлен компонент label

--- components.py
+++ components.py
@@ -52,3 +52,43 @@ class Button:
 
     def set_button_color(self, button_color):
         self.button_color = button_color
+
+
+class Label:
+    def __init__(self, text, pos, font_size, font_color):
+        self.text = text
+        self.pos = pos
+        self.font_size = font_size
+        self.font_color = font_color
+        self.font = pg.font.Font(None, self.font_size)
+        self.image = self.font.render(self.text, True, self.font_color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.pos
+
+    @property
+    def width(self):
+        return self.rect.width
+
+    @property
+    def height(self):
+        return self.rect.height
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    def set_text(self, text):
+        self.text = text
+        self.image = self.font.render(self.text, True, self.font_color)
+
+    def set_pos(self, pos):
+        self.pos = pos
+        self.rect.topleft = self.pos
+
+    def set_font_size(self, font_size):
+        self.font_size = font_size
+        self.font = pg.font.Font(None, self.font_size)
+        self.image = self.font.render(self.text, True, self.font_color)
+
+    def set_font_color(self, font_color):
+        self.font_color = font_color
+        self.image = self.font.render(self.text, True, self.font_color)
