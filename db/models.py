14eb63642d6c6cb489aebb94a33055bc146cdead
Dellyis <nechaev.andrysha@yandex.ru>
2023-03-08 21:04:03 +0700

    refactor(db): remove unused fields

--- db/models.py
+++ db/models.py
@@ -11,24 +11,7 @@ class Upgrades(BaseModel):
     mega_meal: int = 0
 
 
-class Ingredients(BaseModel):
-    anchovy: bool = False
-    avocado: bool = False
-    cheese: bool = True
-    cucumber: bool = False
-    grass: bool = False
-    mango: bool = False
-    mushroom: bool = False
-    olives: bool = False
-    pepper: bool = False
-    pineapple: bool = False
-    sausage: bool = False
-    tomato: bool = False
-    tomato_sauce: bool = True
-
-
 class Data(BaseModel):
     money: int = 0
     day: int = 1
     upgrades: Upgrades = {}
-    ingredients: Ingredients = {}
